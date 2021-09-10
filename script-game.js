57d0942de75e69cd9b103c988d4807360b9bd713
Giovani Bontempo <injuriae+github@gmail.com>
2021-09-09 21:44:30 -0300

    initial

--- script-game.js
+++ script-game.js
@@ -1,17 +1,6 @@
 
-playerSelection = prompt('Escolha Uma das opções: Pedra, Tesoura ou Papael: ');
-
-computerSelection = computerPlay();
-
-playerSelection2 = capitalize(playerSelection);
-
-//converte qualquer entrada do usuário no formato padrão capitalizado
-function capitalize(playerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    let capital = playerSelection.charAt(0).toUpperCase();
-    playerSelection = capital.concat(playerSelection.slice(1,playerSelection.length))
-    return playerSelection
-}
+let playerSelection;
+let computerSelection;
 
 //cria a escolha aleatória do adversário computador
 function computerPlay(){
@@ -21,39 +10,76 @@ function computerPlay(){
 }
 
 //define o resultado do jogo e retorna o resultado
-function round(playerSelection2, computerSelection){
-    if(computerSelection === 'Pedra' && playerSelection2 === 'Tesoura'){
-        return 'Você Perdeu, Pedra quebra Tesoura.'
+function round(playerSelection, computerSelection){
+    if(computerSelection === 'Pedra' && playerSelection === 'Tesoura'){
+        return 'Lose'
     }
-    if(computerSelection === 'Tesoura' && playerSelection2 === 'Papel'){
-        return 'Você Perdeu, Tesoura corta Papel.'
+    if(computerSelection === 'Tesoura' && playerSelection === 'Papel'){
+        return 'Lose'
     }
-    if(computerSelection === 'Papel' && playerSelection2 === 'Pedra'){
-        return 'Você Perdeu, Papel cobre Pedra.'
+    if(computerSelection === 'Papel' && playerSelection === 'Pedra'){
+        return 'Lose'
     }
-    if(playerSelection2 === 'Pedra' && computerSelection === 'Tesoura'){
-        return 'Você Ganhou, Pedra quebra Tesoura.'
+    if(playerSelection === 'Pedra' && computerSelection === 'Tesoura'){
+        return 'Win'
     }
-    if(playerSelection2 === 'Tesoura' && computerSelection === 'Papel'){
-        return 'Você Ganhou, Tesoura corta Papel.'
+    if(playerSelection === 'Tesoura' && computerSelection === 'Papel'){
+        return 'Win'
     }
-    if(playerSelection2 === 'Papel' && computerSelection === 'Pedra'){
-        return 'Você Ganhou, Papel cobre Pedra.'
+    if(playerSelection === 'Papel' && computerSelection === 'Pedra'){
+        return 'Win'
     }
-    else if(playerSelection2 === computerSelection){
+    else if(playerSelection === computerSelection){
         return 'Empate!'
-    } else{
-        return 'Você escolheu uma opção INVÁLIDA!'
-    }
-}
+}};
 
-//cria 5 partidas e reinicializa a escolha do computador
-function game() {
-    for(let i = 0; i <= 5; i++){
-        let result = round(playerSelection2, computerSelection);
-        console.log(`${result} Você escolheu ${playerSelection2} e o computador ${computerSelection}`);
+//manipulador de eventos inline que seleciona o conteúdo do button selecionado
+const choiceButton = (item) => {
+    let button = document.getElementById(item);
+    let contentButton = button.textContent;
+    playerSelection = contentButton; //escolha do jogador igual ao conteúdo do button
+    console.log('VOCE :', playerSelection);
+    rodada(playerSelection);
+};
+
+//faz a rodada de 5x e mostra quem ganhou ou perdeu
+function rodada(playerSelection){
+    computerSelection = computerPlay();
+    let lose = 0, win = 0;
+    let divResul = document.getElementById('result');
+    for(let i = 0; i <=4; i++){
+        console.log('PC :', computerSelection);
+        let roundResult = round(playerSelection, computerSelection);
+        //mostra os resultados na div
+        let paragrafoAdd = document.createElement('p');
+        paragrafoAdd.textContent = `${roundResult}`;
+        divResul.appendChild(paragrafoAdd);
+        //---
+        if(roundResult == "Lose"){
+            lose++
+        }else if(roundResult == 'Win'){
+            win++
+        }
         computerSelection = computerPlay();
-    }
 }
-
-game();
+    if(win > lose){
+        let paragrafoWin = document.createElement('p');
+        paragrafoWin.textContent = "Você GANHOU a rodada";
+        divResul.appendChild(paragrafoWin);
+    }else if(win < lose){
+        let paragrafoLose = document.createElement('p');
+        paragrafoLose.textContent = "Você PERDEU a rodada";
+        divResul.appendChild(paragrafoLose);
+    }else{
+        let paragrafoLose = document.createElement('p');
+        paragrafoLose.textContent = "EMPATOU essa rodada";
+        divResul.appendChild(paragrafoLose);
+    }
+};
+//apaga resultados da rodada
+function erase(){
+    let divResul = document.getElementById('result');
+    for(let i = 0; i <= 5;i++){
+        divResul.removeChild(divResul.lastElementChild);
+    }
+};
